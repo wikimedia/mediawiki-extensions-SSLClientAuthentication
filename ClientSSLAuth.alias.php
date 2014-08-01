f6af9633146503443b366a719cea8e637bfc2fee
Alex Ivanov <alexivanov97@gmail.com>
2014-08-01 22:33:41 +0300

    Create proper aliases file for Special:ClientSSL
    Bug: 69016
    Change-Id: I50f76db1bb9bdecf0cfe65c23591105c96da78ce

---
+++ ClientSSLAuth.alias.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Aliases for Special:ClientSSL
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases[ 'en' ] = array(
+        'ClientSSL' => array( 'ClientSSL' ),
+);
